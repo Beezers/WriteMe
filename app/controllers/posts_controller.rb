9e223f83777fc86952f837e782d32487ee969d5e
Michele Gravina <Michele.Gravina@student.ncirl.ie>
2015-04-28 17:20:48 +0100

    Views for Scenarios added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,31 @@
 class PostsController < ApplicationController
 	def index
+		@content_one = "Some Random Text"
+		@content_two = "Some Random Text Two"
 	end 
-			
+	
+	def new
+	
+	end
+	
+	def create 
+	
+	end 
+	
+	def edit
+	
+	end
+
+	def update
+	
+	end
+	
+	def show	
+	
+	end
+	
+	def destroy
+	
+	end 
+	
 end
