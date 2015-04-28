8639039db583c559c68905ad02a1b3c46750f330
Michele Gravina <Michele.Gravina@student.ncirl.ie>
2015-04-28 20:38:07 +0100

    Updated and implemented CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,18 +4,30 @@ class PostsController < ApplicationController
 	end 
 	
 	def new
-	
+		@post = Post.new
+		@category = Category.all
 	end
 	
 	def create 
-	
+		@post = Post.new(params[:post])
+		if @post.save
+			resirect_to posts_path, :notice =>"Your post has been submitted"
+		else
+		render "new"
+		end
 	end 
 	
 	def edit
-	
+		@post = Post.find(params[:id])
 	end
 
 	def update
+		@post = Post.find(params[:id])
+		if @post.update_attributes!(post_params)
+			redirect_to post_path, :notice =>"Your post has been updated"
+		else
+		render "edit"
+		end
 	
 	end
 	
@@ -27,6 +39,14 @@ class PostsController < ApplicationController
 	
 	def destroy
 	
+	@post = Post.find(params[:id])
+	@post.destroy
+	redirect_to posts_path
 	end 
+	
+	private
+	def post_params    
+    params.require(:post).permit(:title, :body, :category_id)
+	end
 	
 end
